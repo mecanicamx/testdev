777a1190872a1879416fb4ddb2a0d9537ea793f3
Ivan Flores <ivanflores@Ivans-MacBook-Pro.local>
2024-06-06 15:30:16 -0600

    cors

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 //For env File
 dotenv.config();
 console.log(process.env.PORT)
 const app: Application = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT || 8000;
 
 app.post('/myroute', (req: Request, res: Response) => {
